d6bdad05d811bdc9719a690431bfe0d5da660750
singerad <adriansinger87@gmail.com>
2019-05-06 21:42:20 +0200

    code documentation and minor refactoring

---
+++ src/D3vS1m.Domain/System/Exceptions/MatrixException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace D3vS1m.Domain.System.Exceptions
+{
+    /// <summary>
+    /// The specific Exception type for operations with the Matrix class.
+    /// </summary>
+    public class MatrixException : Exception
+    {
+        public MatrixException()
+        {
+        }
+
+        public MatrixException(string message) : base(message)
+        {
+        }
+
+        public MatrixException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MatrixException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
