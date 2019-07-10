36b4c1cc324738ed3c8fbd3970c0f8734153557d
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-07-10 16:05:20 +0200

    change project's name

--- src/D3vS1m.WebUI/ClientApp/components/app-root.vue
+++ src/D3vS1m.WebUI/ClientApp/components/app-root.vue
@@ -1,9 +1,6 @@
 <template>
     <div id="app" class="container-fluid">
         <div class="row">
-            <div class="col-md-3">
-                <nav-menu params="route: route"></nav-menu>
-            </div>
             <div class="col-sm-9">
                 <router-view></router-view>
             </div>
