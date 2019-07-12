216beabbccb2cc68409b79bebe1cef085b6c66a8
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-07-12 12:22:43 +0200

    [IN-PROGRESS] migrate Nav-bar from old project

---
+++ src/D3vS1m.WebUI/ClientApp/components/nav-bar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <div class="nav-wrapper container">
+      <ul class="left">
+        <li>
+          <a id="side-nav-btn" data-activates="slide-out" class="pointer left" style="width: 55px;">
+            <i class="mdi mdi-menu"></i>
+          </a>
+        </li>
+      </ul>
+
+      <a class="brand-logo center">
+        <img class="header-logo left" src="~/images/d3vs1m_logo.svg" />
+        <span class="hide-on-small-only">D3V S1M</span>
+      </a>
+
+      <ul class="right">
+        <li>
+          <a id="start-sim-link" class="waves-effect waves-light">
+            <i class="mdi mdi-play right"></i> Start
+          </a>
+        </li>
+        <li>
+          <a id="console-link" class="modal-trigger waves-effect waves-light" href="#console-modal">
+            <i class="mdi mdi-console left"></i> Console
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
