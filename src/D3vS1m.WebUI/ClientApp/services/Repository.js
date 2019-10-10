ca101694bbffe6d8a62186fd25ce12af256cd378
TK Nguyễn <khanhnpt2127@users.noreply.github.com>
2019-10-10 09:26:57 +0200

    Tk refactor before hackathon (#9)
    * [DONE] change port
    
    * [DONE] remove redundant file in  wwwroot
    
    * [DONE] remove redundant file in wwwroot in UI
    
    * [DONE] change ObjFiles's folder into App_Data

--- src/D3vS1m.WebUI/ClientApp/services/Repository.js
+++ src/D3vS1m.WebUI/ClientApp/services/Repository.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 //TODO: add to GLOBAL VARIBLE deploy dymatically
-const baseDomain = "http://localhost:62363"
+const baseDomain = "http://localhost:52094"
 const baseURL = `${baseDomain}/api`
 
 export default axios.create({
