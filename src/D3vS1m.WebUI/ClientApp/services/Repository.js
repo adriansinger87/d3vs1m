0a0c6a24a91df45823b3dc994e9ac629e14eb349
Adrian Singer <adrian.singer@iwu.fraunhofer.de>
2020-07-13 22:36:20 +0200

    dependencies to web projects removed

--- src/D3vS1m.WebUI/ClientApp/services/Repository.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-//TODO: add to GLOBAL VARIBLE deploy dymatically
-const baseDomain = "http://localhost:62363"
-const baseURL = `${baseDomain}/api`
-
-export default axios.create({
-    baseURL
-})
